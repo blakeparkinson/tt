9a22be4c17a2942f48261833319ca72a7003c582
blakeparkinson <blake@edmodo.com>
2014-08-06 23:56:57 -0700

    first

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Blake Parkinson on 8/2/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
